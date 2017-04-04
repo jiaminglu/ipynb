a6f90d5bf8e162de26b514041b0326d0655e186d
Jiaming Lu <jiaming.lu@ele.me>
2017-04-04 20:07:42 +0800

    add FuncLoader

--- ipynb/utils.py
+++ ipynb/utils.py
@@ -66,20 +66,114 @@ def filter_ast(module_ast):
     module_ast.body = [n for n in module_ast.body if node_predicate(n)]
     return module_ast
 
-def code_from_ipynb(nb, markdown=False):
+
+class IPynbParser(object):
+
+    def __init__(self, filename, enable_params=False):
+        self.filename = filename
+        self.funcname = None
+        self.params = []  # List[(name: str, doc: str)]
+        self.ret = None
+        self.code = []
+        self.doc = []
+        self.enable_params = enable_params
+
+    def parse_code_block(self, lines):
+        """
+        Parse code block, handle #params, #return
+        """
+        iterator = enumerate(lines)
+
+        for i, raw_line in iterator:
+            line = raw_line.strip()
+            parts = line.split(None, 2)
+            if not self.enable_params or \
+                    len(parts) == 0 or \
+                    parts[0] not in ['#param', '#return'] or \
+                    i + 1 >= len(lines): 
+                self.code.append(raw_line)
+                continue
+            doc = parts[1] if len(parts) > 1 else None
+            indent = raw_line[:raw_line.index('#')]
+            if parts[0] == '#param':
+                nextline = next(iterator)[1]
+                code = ast.parse(nextline)
+                if len(code.body) != 1 or not isinstance(code.body[0], ast.Assign):
+                    raise Exception('params must be specified with an assignment')
+                expr = code.body[0]
+                name = expr.targets[0]
+                if isinstance(name, ast.Tuple):
+                    raise Exception('Tuple assignment is not supported for params')
+                if not doc:
+                    doc = 'Defaults to ' + nextline[nextline.index('=') + 1:]
+                self.params.append((name.id, doc))
+                self.code.append(indent + 'if {} is None:\n'.format(name.id))
+                self.code.append(indent + '    {}\n'.format(nextline))
+            elif parts[0] == '#return':
+                ret = next(iterator)[1]
+                self.ret = doc
+                self.code.append(indent + 'return {}\n'.format(ret))
+
+        self.code.append('\n')
+
+    def parse_markdown(self, source):
+        if self.code:
+            self.code.append('\n# ' + '# '.join(source) + '\n')
+        else:
+            self.doc.append(''.join(source))
+
+    def parse(self, nb):
+        for cell in nb['cells']:
+            if cell['cell_type'] == 'code':
+                # transform the input to executable Python
+                # IPython commands should be filtered out
+                lines = [line for line in cell['source']
+                    if not line.strip().startswith('%')]
+                self.parse_code_block(lines)
+            if cell['cell_type'] == 'markdown':
+                self.parse_markdown(cell['source'])
+
+    def get_func_name(self):
+        return self.funcname or self.filename
+
+    def get_doc(self):
+        doc = ''.join(self.doc)
+        doc += '\n'
+        for name, desc in self.params:
+            doc += ':param {}: {}\n'.format(name, desc or '')
+        if self.ret:
+            doc += ':return: {}\n'.format(self.ret)
+        return doc
+
+    def get_param_list(self):
+        return [pair[0] for pair in self.params]
+
+    def get_code(self):
+        doc = self.get_doc()
+
+        if self.params:
+            code = 'def {}({}):\n'.format(
+                self.get_func_name(),
+                ', '.join(map('{}=None'.format, self.get_param_list())),
+            )
+            if doc.strip():
+                code += '    """\n    {}\n    """\n'.format(
+                    doc.replace('\n', '\n    '))
+
+            code += ''.join(map('    {}'.format, self.code))
+            return code
+
+        code = '"""\n{}\n"""\n'.format(doc) if doc.strip() else ''
+        code += ''.join(self.code)
+        return code
+
+
+def code_from_ipynb(nb, markdown=False, filename=None, enable_params=False):
     """
     Get the code for a given notebook
 
     nb is passed in as a dictionary that's a parsed ipynb file
     """
-    code = PREAMBLE
-    for cell in nb['cells']:
-        if cell['cell_type'] == 'code':
-            # transform the input to executable Python, %IPython commands should be filtered out
-            code += ''.join(line for line in cell['source'] if not line.strip().startswith('%'))
-        if cell['cell_type'] == 'markdown':
-            code += '\n# ' + '# '.join(cell['source'])
-        # We want a blank newline after each cell's output.
-        # And the last line of source doesn't have a newline usually.
-        code += '\n\n'
-    return code
+    parser = IPynbParser(filename, enable_params)
+    parser.parse(nb)
+    return parser.get_code()
